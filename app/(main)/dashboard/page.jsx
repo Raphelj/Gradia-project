456850b92a881d5248e55caa4b9e57e085734e6e
Raphelj <rapheljohnson.rap123@gmail.com>
2025-09-13 12:34:45 +0530

    added onboarding page

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
 
-const IndustryInsightsPage = () => {
+import { getUserOnboardingStatus } from "@/actions/user";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  // If not onboarded, redirect to onboarding page
+  // Skip this check if already on the onboarding page
+  if (!isOnboarded) {
+    redirect("/onboarding");
+  }
+
   return (
-    <div>IndustryInsightsPage</div>
-  )
+    <div>IndustryInsights
+    </div>
+  );
 }
-
-export default IndustryInsightsPage
