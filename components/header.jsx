a35df48b0a1a753529cb21781877b12948029d81
Raphelj <rapheljohnson.rap123@gmail.com>
2025-10-01 10:36:49 +0530

    Build resume functionality added

--- components/header.jsx
+++ components/header.jsx
@@ -13,7 +13,7 @@ import { FileText,PenBox,GraduationCap, } from 'lucide-react';
 import { checkUser } from '@/lib/checkUser';
 
 const Header = async() => {
-  checkUser();
+  await checkUser();
 
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
